bc22bbf7f443854122ed26faa87881f5c09850d1
Shu Dong <dongs2011@gmail.com>
2021-12-29 22:54:25 -0800

    add DeMineAgent

---
+++ contracts/DeMineAgent.sol
@@ -0,0 +1,293 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "./DeMineNFT.sol";
+
+contract DeMineAgentCloneFactory {
+    address immutable implementation;
+
+    constructor() {
+        implementation = address(new DeMineAgent());
+    }
+
+    function create(
+        address nft,
+        address costToken,
+        address costRecipient,
+        address owner
+    ) external returns(address) {
+        address cloned = Clones.clone(implementation);
+        DeMineAgent(cloned).initialize(
+            nft,
+            costToken,
+            costRecipient
+        );
+        DeMineAgent(cloned).transferOwnership(owner);
+        return cloned;
+    }
+}
+
+/// @author Shu Dong
+contract DeMineAgent is
+    OwnableUpgradeable,
+    IERC1155Receiver
+{
+    event NewPool(uint128, address, uint256);
+    event Withdraw(address, uint256);
+    event SetSaleInfo(address, address, uint256[], uint256[], uint256[]);
+    event UnsetSaleInfo(address, address, uint256[]);
+    event Claim(address, uint256, uint256, uint256[], uint256[]);
+    event Liquidize(address, uint256, uint256[], uint256[]);
+
+    address private _nft;
+    address private _costToken;
+    address private _costRecipient;
+
+    struct Pool {
+        address issuer;
+        uint256 costPerToken;
+    }
+    mapping(uint128 => Pool) private _pools;
+    mapping(uint256 => uint256) private _balances;
+    struct SaleInfo {
+        uint256 amount;
+        uint256 price;
+    }
+    mapping(address => mapping(uint256 => SaleInfo)) private _selling;
+    mapping(address => uint256) private _income;
+
+    function initialize(
+        address nft,
+        address costToken,
+        address costRecipient
+    ) public initializer {
+        __Ownable_init();
+        _nft = nft;
+        _costToken = costToken;
+        _costRecipient = costRecipient;
+    }
+
+    function newPool(
+        uint128 pool,
+        address issuer,
+        uint256 costPerToken
+    ) external {
+        require(_msgSender() == _nft, "only nft contracts allowed");
+        _pools[pool].issuer = issuer;
+        _pools[pool].costPerToken = costPerToken;
+        emit NewPool(pool, issuer, costPerToken);
+    }
+
+    function setSaleInfo(
+        address recipient,
+        uint256[] calldata ids,
+        uint256[] calldata amounts,
+        uint256[] calldata prices
+    ) external {
+        require(
+            ids.length == amounts.length && amounts.length == prices.length,
+            "array length mismatch"
+        );
+        address sender = _msgSender();
+        require(sender != recipient, "cannot set issuer as recipient");
+        for (uint256 i = 0; i < ids.length; i++) {
+            uint128 pool = uint128(ids[i] >> 128);
+            require(sender == _pools[pool].issuer, "only token issuer allowed");
+            require(
+                prices[i] >= _pools[pool].costPerToken,
+                "price too low to cover cost"
+            );
+            uint256 balance = _balances[ids[i]];
+            uint256 selling = _selling[recipient][ids[i]].amount;
+            require(
+                balance + selling >= amounts[i],
+                "DeMineAgent: insufficient balance to sale"
+            );
+            _balances[ids[i]] = balance + selling - amounts[i];
+            _selling[recipient][ids[i]].amount = amounts[i];
+            _selling[recipient][ids[i]].price = prices[i];
+        }
+        emit SetSaleInfo(sender, recipient, ids, amounts, prices);
+    }
+
+    function unsetSaleInfo(
+        address recipient,
+        uint256[] calldata ids
+    ) external {
+        for (uint256 i = 0; i < ids.length; i++) {
+            uint128 pool = uint128(ids[i] >> 128);
+            require(
+                _msgSender() == _pools[pool].issuer,
+                "only token issuer allowed"
+            );
+            _balances[ids[i]] += _selling[recipient][ids[i]].amount;
+            _selling[recipient][ids[i]].amount = 0;
+        }
+        emit UnsetSaleInfo(_msgSender(), recipient, ids);
+    }
+
+    function claim(
+        uint256[] calldata ids,
+        uint256[] calldata amounts
+    ) external {
+        require(
+            ids.length == amounts.length,
+            "array length mismatch"
+        );
+        address sender = _msgSender();
+        uint256 totalCost;
+        uint256 totalPrice;
+        for (uint256 i = 0; i < ids.length; i++) {
+            uint128 pool = uint128(ids[i] >> 128);
+            uint256 cost = _pools[pool].costPerToken * amounts[i];
+            uint256 price = claim(sender, ids[i], amounts[i]);
+            totalCost += cost;
+            totalPrice += price;
+            _income[_pools[pool].issuer] += (price - cost);
+        }
+        pay(sender, _costRecipient, totalCost);
+        pay(sender, address(this), totalPrice - totalCost);
+        DeMineNFT(_nft).safeBatchTransferFrom(
+            address(this), sender, ids, amounts, ""
+        );
+        emit Claim(sender, totalCost, totalPrice, ids, amounts);
+    }
+
+    function claim(
+        address buyer,
+        uint256 id,
+        uint256 amount
+    ) private returns(uint256) {
+        uint256 allowance1 = _selling[buyer][id].amount;
+        uint256 allowance2 = _selling[address(0)][id].amount;
+        require(
+            allowance1 + allowance2 >= amount,
+            "DeMineAgent: insufficient locked to claim"
+        );
+        if (allowance1 >= amount) {
+            _selling[buyer][id].amount = allowance1 - amount;
+            return amount * _selling[buyer][id].price;
+        } else {
+            _selling[buyer][id].amount = 0;
+            _selling[address(0)][id].amount = allowance1 + allowance2 - amount;
+            return _selling[buyer][id].price * allowance1 +
+                _selling[address(0)][id].price * (amount - allowance1);
+        }
+    }
+
+    function liquidize(
+        uint256[] calldata ids,
+        uint256[] calldata amounts
+    ) external {
+        require(
+            ids.length == amounts.length,
+            "array length mismatch"
+        );
+        uint256 totalCost;
+        for (uint256 i = 0; i < ids.length; i++) {
+            uint128 pool = uint128(ids[i] >> 128);
+            require(
+                _msgSender() == _pools[pool].issuer,
+                "only token issuer allowed"
+            );
+            uint256 fromBalance = _balances[ids[i]];
+            require(
+                fromBalance >= amounts[i],
+                "DeMineAdmin: insufficient balance to liquidize"
+            );
+            _balances[ids[i]] = fromBalance - amounts[i];
+            totalCost += _pools[pool].costPerToken * amounts[i];
+        }
+        pay(_msgSender(), _costRecipient, totalCost);
+        DeMineNFT(_nft).safeBatchTransferFrom(
+            address(this), _msgSender(), ids, amounts, ""
+        );
+        emit Liquidize(
+            _msgSender(),
+            totalCost,
+            ids,
+            amounts
+        );
+    }
+
+    function onERC1155Received(
+        address operator,
+        address from,
+        uint256 id,
+        uint256 amount,
+        bytes calldata
+    ) external override returns (bytes4) {
+        require(
+            operator == _nft && from == address(0),
+            "only newly minted token allowed"
+        );
+        _balances[id] += amount;
+        return bytes4(
+            keccak256(
+                "onERC1155Received(address,address,uint256,uint256,bytes)"
+            )
+        );
+    }
+
+    function onERC1155BatchReceived(
+        address operator,
+        address from,
+        uint256[] calldata ids,
+        uint256[] calldata amounts,
+        bytes calldata
+    ) external override returns (bytes4) {
+        require(
+            operator == _nft && from == address(0),
+            "only newly minted token allowed"
+        );
+        for (uint256 i = 0; i < ids.length; i++) {
+            _balances[ids[i]] += amounts[i];
+        }
+        return bytes4(
+            keccak256(
+                "onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"
+            )
+        );
+    }
+
+    function withdraw(uint256 amount) external {
+        address sender = _msgSender();
+        require(
+            _income[sender] > amount,
+            "insufficient balance to withdraw"
+        );
+        pay(address(this), sender, amount);
+        _income[sender] -= amount;
+        emit Withdraw(sender, amount);
+    }
+
+    function income() external view returns(uint256) {
+        return _income[_msgSender()];
+    }
+
+    function pay(
+        address payer,
+        address payee,
+        uint256 value
+    ) private {
+        if (value > 0) {
+            bool success = IERC20(_costToken).transferFrom(
+                payer, payee, value
+            );
+            require(success, "failed to pay cost");
+        }
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        pure
+        override
+        returns (bool)
+    {
+        return interfaceId == type(IERC1155Receiver).interfaceId;
+    }
+}
--- contracts/DeMineNFT.sol
+++ contracts/DeMineNFT.sol
@@ -2,11 +2,38 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/interfaces/IERC1155Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/interfaces/IERC2981Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/ClonesUpgradeable.sol";
+import "./DeMineAgent.sol";
+
+contract DeMineNFTCloneFactory {
+    address immutable implementation;
+
+    constructor() {
+        implementation = address(new DeMineNFT());
+    }
+
+    function create(
+        string memory uri,
+        address royaltyRecipient,
+        uint16 royaltyBps,
+        address agent,
+        address owner
+    ) external returns(address) {
+        address cloned = Clones.clone(implementation);
+        DeMineNFT(cloned).initialize(
+            uri,
+            royaltyRecipient,
+            royaltyBps,
+            agent
+        );
+        DeMineNFT(cloned).transferOwnership(owner);
+        return cloned;
+    }
+}
 
 /// @author Shu Dong
 contract DeMineNFT is
@@ -15,46 +42,57 @@ contract DeMineNFT is
     IERC2981Upgradeable
 {
     // Events
-    event LogEthDeposit(address);
     event TokenRoyaltySet(uint256);
-    event Reward(uint256, uint256);
+    event NewPool(uint128 indexed, string);
+    event Reward(uint128 indexed, uint256);
+    event RewardWithOverrides(uint128, uint256, uint128[], uint256[]);
     event Redeem(address, uint256);
 
     address private _royaltyRecipient;
     uint16 private _royaltyBps; // EIP2981
 
     address private _rewardToken;
-    address private _nft;
-    uint256 private _nextCycle;
-    mapping(uint256 => uint256) private _reward;
+    address private _agent;
+
+    uint128 private _nextCycle;
+    uint128 private _nextPool;
+    mapping(uint128 => uint256) private _reward;
+    mapping(uint256 => uint256) private _overrides;
 
     function initialize(
         string memory uri,
         address royaltyRecipient,
-        uint16 royaltyBps
+        uint16 royaltyBps,
+        address agent
     ) public initializer {
         __Ownable_init();
         __ERC1155_init(uri);
         _royaltyRecipient = royaltyRecipient;
         _royaltyBps = royaltyBps;
+        _agent = agent;
     }
 
     constructor() initializer {}
 
-    function mint(
-        uint256 startCycle,
-        uint256 numCycles,
+    function newPool(
+        string calldata info,
+        uint128 startCycle,
+        uint128 numCycles,
         uint256 supplyPerCycle,
-        address recipient
+        uint256 costPerToken,
+        address issuer
     ) external onlyOwner {
         require(startCycle > _nextCycle, "cannot start from past");
-        uint256[] memory cycles = new uint256[](numCycles);
+        uint256[] memory ids = new uint256[](numCycles);
         uint256[] memory supplies = new uint256[](numCycles);
         for (uint256 i = 0; i < numCycles; i++) {
-            cycles[i] = i + startCycle;
+            ids[i] = (uint256(_nextPool) << 128) + i + startCycle;
             supplies[i] = supplyPerCycle;
         }
-        _mintBatch(recipient, cycles, supplies, "");
+        DeMineAgent(_agent).newPool(_nextPool, issuer, costPerToken);
+        _mintBatch(_agent, ids, supplies, "");
+        emit NewPool(_nextPool, info);
+        _nextPool += 1;
     }
 
     function reward(uint256 expectedRewardPerToken) external onlyOwner {
@@ -63,23 +101,28 @@ contract DeMineNFT is
         _nextCycle += 1;
     }
 
-    function redeem(
-        uint256[] calldata cycles,
-        uint256[] calldata amounts
-    ) external {
-        _burnBatch(_msgSender(), cycles, amounts);
-        uint256 totalReward;
-        for (uint256 i = 0; i < cycles.length; i++) {
-            require(cycles[i] < _nextCycle, "unrewarded cycle");
-            totalReward += amounts[i] * _reward[cycles[i]];
+    function rewardWithOverrides(
+        uint128 expectedRewardPerToken,
+        uint128[] calldata pools,
+        uint256[] calldata overrides
+    ) external onlyOwner {
+        require(
+            pools.length == overrides.length,
+            "array length mismatch"
+        );
+        for (uint256 i = 0; i < pools.length; i++) {
+            _overrides[
+                (uint256(pools[i]) << 128) + _nextCycle
+            ] = overrides[i];
         }
-        if (totalReward > 0) {
-            bool success = IERC20(_rewardToken).transfer(
-                _msgSender(), totalReward
-            );
-            require(success, "failed to withdraw reward");
-        }
-        emit Redeem(_msgSender(), totalReward);
+        _reward[_nextCycle] = expectedRewardPerToken;
+        emit RewardWithOverrides(
+            _nextCycle,
+            expectedRewardPerToken,
+            pools,
+            overrides
+        );
+        _nextCycle += 1;
     }
 
     function setTokenRoyaltyInfo(
@@ -89,6 +132,28 @@ contract DeMineNFT is
         _royaltyRecipient = recipient;
         _royaltyBps = bps;
         emit TokenRoyaltySet(bps);
+    }
+
+    function redeem(
+        uint256[] calldata ids,
+        uint256[] calldata amounts
+    ) external {
+        _burnBatch(_msgSender(), ids, amounts);
+        uint256 totalReward;
+        for (uint256 i = 0; i < ids.length; i++) {
+            uint128 cycle = uint128(ids[i]);
+            require(cycle < _nextCycle, "unrewarded cycle");
+            totalReward += amounts[i] * (
+                _overrides[ids[i]] > 0 ? _overrides[ids[i]] : _reward[cycle]
+            );
+        }
+        if (totalReward > 0) {
+            bool success = IERC20(_rewardToken).transfer(
+                _msgSender(), totalReward
+            );
+            require(success, "failed to withdraw reward");
+        }
+        emit Redeem(_msgSender(), totalReward);
     }
 
     // view functions
