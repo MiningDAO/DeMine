728b8f5a7d2015d6cf4f2e952571da5bbf724347
Shu Dong <dongs2011@gmail.com>
2022-01-25 16:14:02 -0800

    add user inspection

--- tasks/inspect.js
+++ tasks/inspect.js
@@ -6,41 +6,43 @@ task('inspect-user', 'Inspect state of DeMineNFT contract')
     .addParam('who', 'account address')
     .addParam('what', 'data to inspect')
     .addParam('start', 'start token id')
-    .addParam('end', 'end token id')
+    .addOptionalParam('end', 'end token id')
     .setAction(async (args, { ethers, network, localConfig }) => {
         assert(network.name !== 'hardhat', 'Not supported at hardhat network');
 
         let nft = localConfig[network.name][args.coin].nft;
         const erc1155Facet = await ethers.getContractAt('ERC1155Facet', nft);
+        const account = ethers.utils.getAddress(args.who);
+
         if (args.what == 'balance') {
-            assert(args.end > args.start, 'end must be larger than end');
-            assert(args.end - args.start > 365, 'you can only check one-year data');
+            assert(args.end && args.end >= args.start, 'end must be larger than start');
+            assert(args.end - args.start <= 365, 'you can only check one-year data');
             const balances = await erc1155Facet.balanceOfBatch(
-                Array(args.end - args.start + 1).fill(args.account), // accounts
-                [...Array(args.end - args.start + 1).keys()].map(i => i + start) // ids
+                Array(args.end - args.start + 1).fill(account), // accounts
+                [...Array(args.end - args.start + 1).keys()].map(i => i + args.start) // ids
             );
-            const result = {total: 0, tokens: []};
-            for (let i = 0; i < ids.length; i++) {
-                result.total += balance[i];
-                result.tokens.push({
-                    tokenId: ids[i],
-                    balance: balances[i].toNumber()
-                });
-            }
-            console.log(JSON.stringify(result, null, 2));
+            const total = balances.reduce((prev, cur) => cur.add(prev));
+            const result = {
+                total: total.toNumber(),
+                balances: balances.map(b => b.toNumber())
+            };
+            console.log(JSON.stringify({
+                total: result.total,
+                balances: JSON.stringify(result.balances)
+            }, null, 2));
             return result;
         }
 
-        if (arg.what == 'income') {
+        if (args.what == 'income') {
             const mining = (await erc1155Facet.getMining()).toNumber();
             assert(
                 args.start < mining,
                 'start has to be lower than current mining ' + mining
             );
-            var ids = [...Array(mining - args.start).keys()].map(i => i + start);
+            var ids = [...Array(mining - args.start).keys()].map(i => i + args.start);
             var tokenInfo = await erc1155Facet.getTokenInfo(ids);
             var balances = await erc1155Facet.balanceOfBatch(
-                Array(mining - args.start).fill(args.account), // accounts
+                Array(mining - args.start).fill(account), // accounts
                 ids
             );
             var result = {total: 0, perToken: []};
@@ -51,12 +53,14 @@ task('inspect-user', 'Inspect state of DeMineNFT contract')
                 result.perToken.push({
                     tokenId: i,
                     balance: balance.toNumber(),
-                    income: info[1]
+                    incomePerToken: info[1].toNumber()
                 })
             }
             console.log(JSON.stringify(result, null, 2));
             return result;
         }
+
+        console.log('invalid `what` param, only `balance` and `income` are supported');
     });
 
 task('inspect-nft', 'Inspect state of DeMineNFT contract')
@@ -66,30 +70,32 @@ task('inspect-nft', 'Inspect state of DeMineNFT contract')
         common.validateCoin(args.coin);
 
         let nft = localConfig[network.name][args.coin].nft;
-        const adminFacet = await ethers.getContractAt('DeMineNFT', nft);
-        const diamondFacet = await ethers.getContractAt('DiamondFacet', nft);
         const erc1155Facet = await ethers.getContractAt('ERC1155Facet', nft);
+        const mining = (await erc1155Facet.getMining()).toNumber();
+        var history = [];
+        var start = Math.max(mining - 5, 0);
+        if (mining > start) {
+            var tokenInfo = await erc1155Facet.getTokenInfo(
+                [...Array(mining - start).keys()].map(i => i + start)
+            );
+            for (let i = start; i < mining; i++) {
+                let info = tokenInfo[i - start];
+                history.push({
+                    tokenId: i,
+                    supply: info[0].toNumber(),
+                    income: info[1].toNumber()
+                });
+            }
+        }
+
+        const adminFacet = await ethers.getContractAt('DeMineNFT', nft);
         const royaltyInfo = await erc1155Facet.royaltyInfo(1, 10000);
         const miningPoolFacet = await ethers.getContractAt('MiningPoolFacet', nft);
         const income = await ethers.getContractAt(
             'DeMineERC20', await miningPoolFacet.treasureSource()
         );
-        const mining = (await erc1155Facet.getMining()).toNumber();
-        const [miningToken] = await erc1155Facet.getTokenInfo([mining]);
         const balance = await income.balanceOf(nft);
-        var history = [];
-        var start = Math.max(mining - 5, 0);
-        var tokenInfo = await erc1155Facet.getTokenInfo(
-            [...Array(mining - start).keys()].map(i => i + start)
-        );
-        for (let i = start; i <= mining; i++) {
-            let info = tokenInfo[i - start];
-            history.push({
-                tokenId: i,
-                supply: info[0].toNumber(),
-                income: info[1].toNumber()
-            });
-        }
+        const [miningToken] = await erc1155Facet.getTokenInfo([mining]);
         console.log(JSON.stringify({
             address: nft,
             owner: await adminFacet.owner(),
@@ -111,7 +117,6 @@ task('inspect-nft', 'Inspect state of DeMineNFT contract')
             royaltyInfo: {
                 recipient: royaltyInfo[0],
                 bps: royaltyInfo[1].toNumber(),
-            },
-            facets: await diamondFacet.facets
+            }
         }, undefined, 2));
     });
