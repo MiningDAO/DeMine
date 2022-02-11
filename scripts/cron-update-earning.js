cfa7958813abe041622185ce4ddc994e16ae9d0a
Shu Dong <dongs2011@gmail.com>
2022-02-11 09:49:23 -0800

    add earning cron job

--- scripts/cron-update-earning.js
+++ scripts/cron-update-earning.js
@@ -1,5 +1,8 @@
-var cron = require('node-cron');
+const { ethers } = hre = require("hardhat");
+const { load } = require('../lib/bootstrap.js');
+const cron = require('node-cron');
+const redis = require('redis').createClient();
 
-cron.schedule('* * 8 * *', () => {
-  console.log('running 8am every day');
+cron.schedule('0 0 */24 * *', async () => {
+    await load('btc', redis);
 });
