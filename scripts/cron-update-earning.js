6596a7508da4f3bcfde8a35a10f26b64e94789d7
shudong <dongs2011@gmail.com>
2022-02-11 18:06:46 +0000

    embed cron job to node server

--- scripts/cron-update-earning.js
+++
@@ -1,8 +0,0 @@
-const { ethers } = hre = require("hardhat");
-const { load } = require('../lib/bootstrap.js');
-const cron = require('node-cron');
-const redis = require('redis').createClient();
-
-cron.schedule('0 0 */24 * *', async () => {
-    await load('btc', redis);
-});
