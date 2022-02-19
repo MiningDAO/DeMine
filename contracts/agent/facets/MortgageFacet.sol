c21d7de250352a2912d22a4fbd92970295a3d6a9
yijiehong <yijiehong@yunjings-mbp.lan>
2022-02-18 22:55:32 -0800

    Remove the deposit from mortgage agent and take off-chain. Also, change payoff to withdrawAndPayoff which takes a list of token IDs

--- contracts/agent/facets/MortgageFacet.sol
+++ contracts/agent/facets/MortgageFacet.sol
@@ -61,17 +61,14 @@ contract MortgageFacet is
             ids.length == amounts.length,
             "DeMineAgent: array length mismatch");
         uint tokenCost = s.tokenCost;
-        uint128 finalizedEnd = s.finalizedEnd;
-
-        Account memory account = s.accounts[msg.sender];
+        uint128 billedTime = s.billedTime;
 
         uint totalCost;
         for (uint i = 0; i < ids.length; i++) {
             require(
-                tokenIdToStart(ids[i]) >= finalizedEnd,
+                tokenIdToStart(ids[i]) >= billedTime,
                 "DeMineAgent: cannot redeem NFT for ongoing/completed mining.");
-            require(isWeeklyToken(ids[i]), "DeMineAgent: Only weekly token is supported");
-            totalCost += tokenCost * amounts[i] * daysInWeek();
+            totalCost += tokenCost * amounts[i] * daysInToken(ids[i]);
             uint balance = s.balances[ids[i]][msg.sender];
             require(balance > 0 && balance > amounts[i], 'DeMineAgent: no sufficient balance');
             s.balances[ids[i]][msg.sender] = balance - amounts[i];
@@ -81,50 +78,6 @@ contract MortgageFacet is
         }
         emit Redeem(msg.sender, ids, amounts);
         s.nft.safeBatchTransferFrom(address(this), msg.sender, ids, amounts, "");
-    }
-
-    /**
-     * @notice adjust accounts for msg sender for valid weeklyTokenLockStart and
-     *         weeklyTokenLockEnd times for msg.sender to prevent infinite loop.
-     *         weeklyTokenLockStart and weeklyTokenLockEnd could be inaccurate
-     *         after redeemNFT().
-     */
-    function correctAccount() external whenNotPaused {
-        Account memory current = s.accounts[msg.sender];
-        Account memory update = Account(
-            current.weeklyTokenLockStart,
-            current.weeklyTokenLockEnd);
-
-        // Correct account for weekly token.
-        uint256 lockedWeeklyToken = getWeeklyTokenFromStartTime(update.weeklyTokenLockStart);
-        while(tokenIdToEnd(lockedWeeklyToken) <= update.weeklyTokenLockEnd) {
-            if (s.balances[lockedWeeklyToken][msg.sender] == 0 &&
-                update.weeklyTokenLockStart <= update.weeklyTokenLockEnd) {
-                update.weeklyTokenLockStart += 604800;
-            } else {
-                break;
-            }
-            lockedWeeklyToken = nextWeeklyTokenId(lockedWeeklyToken);
-        }
-        lockedWeeklyToken = getWeeklyTokenFromEndTime(update.weeklyTokenLockEnd);
-        while(tokenIdToStart(lockedWeeklyToken) >= update.weeklyTokenLockStart) {
-            if (s.balances[lockedWeeklyToken][msg.sender] == 0 &&
-                update.weeklyTokenLockStart <= update.weeklyTokenLockEnd) {
-                update.weeklyTokenLockEnd -= 604800;
-            } else {
-                break;
-            }
-            lockedWeeklyToken = previousWeeklyTokenId(lockedWeeklyToken);
-        }
-        updateAccount(msg.sender, current, update);
-    }
-
-    /**
-     * @notice get account info
-     * @param account Address of account to query
-     */
-    function getAccountInfo(address account) external view returns(Account memory) {
-        return s.accounts[account];
     }
 
     function balanceOfBatch(
@@ -163,64 +116,42 @@ contract MortgageFacet is
             'DeMineAgent: only minted tokens from DeMineNFT allowed'
         );
         (address mortgager) = abi.decode(data, (address));
-        Account memory current = s.accounts[mortgager];
-        Account memory update = Account(
-            current.weeklyTokenLockStart,
-            current.weeklyTokenLockEnd);
         for (uint i = 0; i < ids.length; i++) {
-            require(isWeeklyToken(ids[i]), "DeMineAgent: Only weekly token is supported");
             uint balance = s.balances[ids[i]][mortgager] + amounts[i];
             s.balances[ids[i]][mortgager] = balance;
-
-            update.weeklyTokenLockStart = Util.uint128min2(tokenIdToStart(ids[i]), update.weeklyTokenLockStart);
-            update.weeklyTokenLockEnd = Util.uint128max2(tokenIdToEnd(ids[i]), update.weeklyTokenLockEnd);
         }
-        updateAccount(msg.sender, current, update);
         return IERC1155Receiver.onERC1155BatchReceived.selector;
     }
 
     /**
-     * @notice payoff debt from start to end. Ensure you have a
-     *         valid start set to prevent infinite loop
+     * @notice Withdraw surplus or payoff debt for NFT tokens that have been billed.
+     *         All balances of ids need to be paid off or withdrawn. Partial payoff
+     *         or withdraw is not supported.
      */
-    function payoff(uint128 end) external whenNotPaused {
+    function withdrawAndPayoff(
+        uint256[] calldata ids
+    ) external whenNotPaused {
         uint income;
         uint debt;
-        require(end % 86400 == 0, "DeMineAgent: invalid payoff end time");
-        Account memory account = s.accounts[msg.sender];
-        require(end <= s.finalizedEnd, 'DeMineAgent: cannot payoff unfinalized NFTs');
+        uint128 billedTime = s.billedTime;
 
-        // Payoff weekly token
-        uint256 lockedWeeklyToken = getWeeklyTokenFromStartTime(account.weeklyTokenLockStart);
-        while(tokenIdToEnd(lockedWeeklyToken) <= end) {
-            uint balance = s.balances[lockedWeeklyToken][msg.sender];
-            if (balance > 0) {
-                uint128 lastDayEnd = tokenIdToEnd(lockedWeeklyToken);
-                uint128 firstDayEnd = lastDayEnd - (uint128(daysInWeek()) - 1) * 86400;
-                for (uint128 dailyEnd = firstDayEnd; dailyEnd<= lastDayEnd; dailyEnd + 86400) {
-                    DailyStatement memory st = s.statements[dailyEnd];
-                    income += st.surplus * balance / st.balance;
-                    debt += st.debt * balance / st.balance;
-                }
-                s.balances[lockedWeeklyToken][msg.sender] = 0;
-            }
-            lockedWeeklyToken = nextWeeklyTokenId(lockedWeeklyToken);
+        for (uint i = 0; i < ids.length; i++) {
+            require(
+                tokenIdToEnd(ids[i]) <= billedTime,
+                "DeMineAgent: cannot withdraw or payoff NFT for not yet billed mining.");
+            BillingStatement memory st = s.statements[ids[i]];
+            uint balance = s.balances[ids[i]][msg.sender];
+            income += st.surplus * balance / st.balance;
+            debt += st.debt * balance / st.balance;
+            delete s.balances[ids[i]][msg.sender];
         }
-        s.accounts[msg.sender].weeklyTokenLockStart = tokenIdToStart(lockedWeeklyToken);
 
         if (debt > 0) {
             s.paymentToken.safeTransferFrom(s.payee, msg.sender, debt);
         }
-        s.incomeToken.safeTransfer(msg.sender, income);
-    }
-
-    function updateAccount(
-        address account,
-        Account memory current,
-        Account memory update
-    ) private {
-        s.accounts[account].weeklyTokenLockStart = update.weeklyTokenLockStart;
-        s.accounts[account].weeklyTokenLockEnd = update.weeklyTokenLockEnd;
+        if (income > 0) {
+            s.incomeToken.safeTransfer(msg.sender, income);
+        }
     }
 
     function tokenIdToStart(uint256 tokenId) private pure returns(uint128) {
@@ -231,29 +162,7 @@ contract MortgageFacet is
         return uint128(tokenId);
     }
 
-    function isWeeklyToken(uint256 tokenId) private pure returns(bool) {
-        return (tokenIdToEnd(tokenId) - tokenIdToStart(tokenId)) == 604800;
-    }
-
-    function daysInWeek() private pure returns(uint) {
-        return 7;
-    }
-
-    // nextWeeklyTokenId assumes weekly tokens are created every 7 days.
-    function nextWeeklyTokenId(uint256 tokenId) private pure returns(uint256) {
-        return uint256(uint256(tokenIdToEnd(tokenId)) << 128 | (tokenIdToEnd(tokenId) + 604800));
-    }
-
-    // previousWeeklyTokenId assumes weekly tokens are created every 7 days.
-    function previousWeeklyTokenId(uint256 tokenId) private pure returns(uint256) {
-        return uint256(uint256(tokenIdToStart(tokenId) - 604800) << 128 | (tokenIdToStart(tokenId)));
-    }
-
-    function getWeeklyTokenFromStartTime(uint128 start) private pure returns(uint256) {
-        return nextWeeklyTokenId(uint256(start));
-    }
-
-    function getWeeklyTokenFromEndTime(uint128 end) private pure returns(uint256) {
-        return previousWeeklyTokenId(uint256(end) << 128);
+    function daysInToken(uint256 tokenId) private pure returns(uint) {
+        return (tokenIdToEnd(tokenId) - tokenIdToStart(tokenId)) / 86400;
     }
 }
--- contracts/agent/lib/AppStorage.sol
+++ contracts/agent/lib/AppStorage.sol
@@ -7,12 +7,7 @@ import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
 import '../../nft/interfaces/IERC1155Rewardable.sol';
 
-struct Account {
-    uint128 weeklyTokenLockStart;  // The start timestamp of the first locked weekly token in the agent
-    uint128 weeklyTokenLockEnd;  // The start timestamp of the first locked daily token in the agent
-}
-
-struct DailyStatement {
+struct BillingStatement {
     uint balance;  // Today's total locked (not liquidated) NFT for today
     uint surplus;  // Today's surplus (mined target tokens - the part to payoff unpaid cost) in incomeToken
     uint debt;  // The debt in paymentToken if today's mined target tokens cannot pay off cost
@@ -27,9 +22,9 @@ struct AppStorage {
     address payee; // payee account address, could be contract
 
     bool initialized;
-    uint128 finalizedEnd; // The end timestamp of the last day being finalized
-    mapping(uint128 => DailyStatement) statements; // Daily statement. mapping(end_timestamp => DailyStatement) 
-    mapping(address => Account) accounts;  // account info
+    uint128 billedTime; // The end timestamp of the last day being billed
+    mapping(uint256 => BillingStatement) statements; // Billing statement for each NFT token.
+                                                     // mapping(end_timestamp => BillingStatement)
 
     mapping(uint => mapping(address => uint)) balances;  // owner => buyer => allowance
     mapping(address => mapping(address => mapping(uint => uint))) allowances;
